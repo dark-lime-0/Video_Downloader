f05ec3e844119456ef4d4160111578c31aa710dd
Youssef <96352958+dark-lime-0@users.noreply.github.com>
2024-04-23 00:44:13 +0100

    Update Youtube_Video_Downloader.py
    UPD Project

--- Youtube_Video_Downloader.py
+++ Youtube_Video_Downloader.py
@@ -1,19 +1,19 @@
 from pytube import YouTube
+print("HI!! This is a Simple Script to Download a Video From YouTube!!")
 
-print("HI THERE This is a Simple Script to Download a Video From YouTube!!")
-
-# Enter the YouTube video URL
-video_url = input("Enter the video URL: ")
-
+# YT video URL
+video_url = input("# ")
+path=input("PATH: ")
 try:
-    # Create a YouTube object with the video URL
     yt = YouTube(video_url)
+    print("title:", yt.title)
+    print("views:", yt.views)
 
-    # Select the highest quality video stream and download it
     stream = yt.streams.get_highest_resolution()
-    stream.download()
 
-    print("The Video is Downloaded Successfully !!")
+    stream.download(path)
+    print("Done !!")
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
+
